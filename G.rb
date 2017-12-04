8d8c9918a4ad97ae5df52d472567ee0827a06ab3
Max Motz <mmotz@browning.edu>
2017-12-04 07:26:34 +0000

    Part 1

---
+++ G.rb
@@ -0,0 +1,314 @@
+
+class Scene
+  def enter()
+    puts "This scene is not yet configured. Subclass it and implement enter()."
+    exit(1)
+  end
+end
+
+class Engine
+
+  def initialize(scene_map)
+    @scene_map = scene_map
+  end
+
+  def play()
+    current_scene = @scene_map.opening_scene()
+    last_scene = @scene_map.next_scene('finished')
+
+    while current_scene != last_scene
+      next_scene_name = current_scene.enter()
+      current_scene = @scene_map.next_scene(next_scene_name)
+    end
+
+    # be sure to print out the last scene
+    current_scene.enter()
+  end
+end
+
+class SpaceFinal < Scene
+  def enter()
+    puts "\n"
+    puts "You've arrived at the Browning school"
+    puts "Or what's left of it, atleast..."
+    puts "You see the head of the school, Dr. soul, weeping on the ground"
+    puts "He Yells, 'THIS WASN'T SUPPOSED TO BE LIKE THIS,"
+    puts "O WHAT MISERY!'"
+    puts "He scrambles up to you and grabs you by the collar hysterically"
+    puts "'Your Browning, along with space and time's FINAL HOPE,' he blurts out"
+    puts "'PLEASE, use this time machine and save my school'"
+    puts "What do you do:"
+    puts "Accept"
+    puts "Decline"
+    puts "Ask him why he cannot do it himself"
+    meaningless = $stdin.gets.chomp
+    puts "\n"
+    puts "Before you could respond to his plight, he throws you into"
+    puts "this time machine and you appear in front of the Browning school"
+    puts "However, this seems like an actually ordinary day"
+    puts "FIN PART 1"
+    puts "THANK YOU FOR PLAYING"
+    puts "PART 2 COMING EVENTUALLY ;)"
+    exit(1)
+  end
+end
+class Space2 < Scene
+  def enter()
+    puts "\n"
+    puts "What will you do to with these hoodlums:"
+    puts "Fight them"
+    puts "Converse with them"
+    puts "Eavesdrop on their conversation"
+    puts "Ignore them"
+  spaceact2 = $stdin.gets.chomp
+    if spaceact2 == "1"
+      puts "\n"
+      puts "You throw a punch at the closest one"
+      puts "You sucessfull hit him, however his friend takes out"
+      puts "A shotgun blows your head off"
+      return 'death'
+    elsif spaceact2 == "2"
+      puts "\n"
+      puts "You say hi and introduce yourself"
+      puts "They all turn around to look at you, then"
+      puts "They all scatter out of there"
+      puts "Oh well, you continue toward the school"
+      return 'sdfinal'
+    elsif spaceact2 == "3"
+      puts "\n"
+      puts "You sneak up to their huddle, close enough to hear their whispers"
+      puts "'HAHAH, we actually DID IT, we destroyed space and time along with the Browning School'"
+      puts "One of them says"
+      puts "His friends cackle with him"
+      puts "'I can't believe it was that EASY'"
+      puts "The 2nd one scoffs"
+      puts "'Yeah, not a single person tried to stop us'"
+      puts "The 3rd one YAHOOs out of his mouth"
+      puts "The 1st one starts doing a dance, but ends up noticing you,"
+      puts "which causes him and the others to scatter"
+      puts "You continue your journey toward the school"
+      return 'sdfinal'
+    elsif spaceact2 == "4"
+      puts "\n"
+      puts "You ignore the hooded figures and continue towards the school"
+      return 'sdfinal'
+    else 
+      return 'sd2'
+end
+end
+class Space < Scene
+  def enter()
+    puts "\n"
+    puts "You are on a platform of land with your house"
+    puts "There are other platforms around, as well as an abyss below you"
+    puts "Do you:"
+    puts "Jump one by one to each platform, attempting to get to school"
+    puts "Jump into the void"
+    puts "Go inside and call it a sick day"
+    spaceact = $stdin.gets.chomp
+      if spaceact == "1"
+        puts "\n"
+        puts "You sucessfully jump to the next platform"
+        puts "And the next one"
+        puts "And th- WAIT"
+        puts "There are three hooded figures standing on the platform in front of you"
+        puts "They don't seem to notice you as they are hudled together"
+        return 'sd2'
+      elsif spaceact == "2"
+      puts "\n"
+        puts "You died, what didja think would happen?"
+        return 'death'
+      elsif space act == "3"
+      puts "\n"
+        puts "You go inside, as if nothing ever happened"
+        puts "YOU WIN"
+      else
+        return 'sd'
+  end
+end
+
+class Death < Scene
+
+  def enter()
+    puts "GAME OVER, YOU LOSE"
+    exit(1)
+  end
+end
+
+class Bathroom < Scene
+  
+def enter()
+  puts "\n"
+  puts "Would you like to:"
+  puts "Brush your teeth"
+  puts "Take a Shower"
+  puts "Go to the toilet"
+  puts "leave"
+  
+  bathact = $stdin.gets.chomp
+  
+    if bathact == "1"
+      puts "clean and white and very nice"
+      return 'bathroom'
+    elsif bathact == "2"
+      puts "Your squeaky clean now"
+      return 'bathroom'
+    elsif bathact == "3"
+      puts "Kek, you did your business"
+      return 'bathroom'
+    elsif bathact == "4"
+      return 'bedroom'
+    else
+      puts "?"
+      return 'bathroom'
+  end
+end
+
+class Droom < Scene
+  
+def enter()
+  puts "\n"
+  puts "What would you like to eat"
+  puts "Milk and cereal"
+  puts "Cereal and milk"
+  puts "Waffles"
+  puts "a singular whole banana"
+  puts "Omlette"
+  puts "Leave"
+  
+  dract = $stdin.gets.chomp
+  
+    if dract == "1" || "2" || "5"
+      puts "OMNOMNOM"
+      return 'bedroom'
+    elsif dract == "4"
+      puts "You 'accidentally' put the whole banana in your throat and choke and die."
+      return 'death'
+    elsif dract == "3"
+      puts "DO YOU LIKE WAFFLES?"
+      puts "YEAH WE LIKE WAFFLES!"
+      return 'bedroom'
+    elsif dract == "6"
+      return 'bedroom'
+    else
+      puts "?"
+      return 'droom'
+  end
+end
+
+
+class Bedroom < Scene
+  
+  def enter()
+    puts "\n"
+    puts "Your in your room, what do you do:"
+    puts "1. Get dressed"
+    puts "2. Go back to Sleep"
+    puts "3. Go to the Bathroom"
+    puts "4. Go to the Diningroom"
+    puts "5. Leave the Building"
+    puts "Answer should be given as number of choice"
+    
+    braction = $stdin.gets.chomp
+    
+     if braction == "1"
+      puts "\n"
+      puts "What do you want to wear?"
+      puts "Something Plain"
+      puts "Something swanky"
+      puts "A sweatshirt"
+      braction2 = $stdin.gets.chomp
+        if braction2 == "1"
+          puts "You choose a white shirt and a red tie."
+          return 'bedroom'
+        elsif braction2 == "2"
+          puts "You choose a bright purple shirt and a bright green sparkly bow tie."
+          return 'bedroom'
+        elsif braction2 == "3"
+          puts "You put on a red sweatshirt with the Browning logo on it"
+          return 'bedroom'
+        else 
+        puts "Guess ya don't change?"
+        return 'bedroom'
+      end
+
+    elsif braction == "2"
+      puts "You decide to go back to bed cuz this day is a waste of your time."
+      puts  "zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz"
+      puts  "zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz.........."
+      puts  "z?"
+      puts  "You sleep through the whole day and that was that..."
+      puts  "YOU WIN! CONGRATULATIONS"
+      exit(1)
+
+    elsif braction == "3"
+      return 'bathroom'
+      
+    elsif braction == "4"
+      return 'droom'
+    elsif braction == "5"
+      puts "\n"
+      puts "You start walking to school, but realize"
+      puts "That space and time have fallen appart"
+      puts "Instead of the world being how you once knew it to be"
+      puts "It is shreded into billions of different pieces"
+      return 'sd'
+    else 
+      return 'broom'
+    end
+  end
+end
+
+class CentralCorridor < Scene
+
+  def enter()
+    puts "An Ordinary Day at the Browning School Part 1"
+    puts "By Maximilian Motz"
+    puts "\n"
+    puts "DISCLAIMER: Every character in this game is NOT a real person at Browning in order for me to avoid conflict."
+    puts "If you think you weren't accurately portrayed in this game, your wrong cuz its NOT YOU!"
+    puts "BTW the player is YOU, not the fictional version of me or otherwise"
+    puts "\n"
+    puts "You awaken"
+    puts "Lucky you, its panther day (this is fiction btw) at Browning and you have no classes!"
+    puts "Don't get the wrong idea though, your momma's still gonna make you go to school..."
+    return 'bedroom'
+  end
+end
+
+
+class Map
+  @@scenes = {
+    'central_corridor' => CentralCorridor.new(),
+    'death' => Death.new(),
+    'bedroom' => Bedroom.new(),
+    'bathroom' => Bathroom.new(),
+    'droom' => Droom.new(),
+    'sd' => Space.new(),
+    'sd2' => Space2.new(),
+    'sdfinal' =>SpaceFinal.new()
+  }
+
+
+  def initialize(start_scene)
+    @start_scene = start_scene
+  end
+
+
+  def next_scene(scene_name)
+    val = @@scenes[scene_name]
+    return val
+  end
+
+  def opening_scene()
+    return next_scene(@start_scene)
+  end
+end
+
+a_map = Map.new('central_corridor')
+a_game = Engine.new(a_map)
+a_game.play()
+end
+end
+end
+end
---
+++ myTextBasedGameDialogue.rb
@@ -0,0 +1,323 @@
+
+#Title
+puts "An Ordinary Day at the Browning School"
+puts "By Maximilian Motz"
+
+#Disclaimer
+puts "DISCLAIMER: Every character in this game is NOT a real person at Browning in order for me to alter their personality and name to avoid conflict."
+puts "If you think you weren't accurately portrayed in this game, your wrong cuz its NOT YOU!"
+puts "BTW the player is YOU, not the fictional version of me or otherwise"
+
+#Start
+puts "You awaken"
+puts "Lucky you, its panther day (this is fiction btw) at Browning and you have no classes!"
+puts "Don't get the wrong idea though, your momma's still gonna make you go to school..."
+
+#Bedroom choice
+puts "Your in your room, what do you do?"
+puts "Get dressed"
+puts "Go back to Sleep"
+puts "Go to the Bathroom"
+puts "Go to the Diningroom"
+puts "Leave the Building"
+
+#Clothing choice
+puts "What do you want to wear?"
+puts "Something Plain"
+puts "Something swanky"
+puts "A sweatshirt"
+
+#Getting dressed
+puts "You choose a white shirt and a red tie."
+puts "You choose a bright purple shirt and a bright green sparkly bow tie."
+puts "You put on a red sweatshirt with the Browning logo on it"
+
+#Go back to sleep
+puts "You decide to go back to bed cuz this day is a waste of your time."
+puts "zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz"
+puts "zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz.........."
+puts "z?"
+puts "You sleep through the whole day and that was that..."
+puts "YOU WIN!"
+puts "YOU GOT THE: skip school 4 sleep ending"
+
+#Go to the Bathroom
+puts "Would you like to:"
+puts "Brush your teeth"
+puts "Take a Shower"
+puts "Go to the toilet"
+puts "leave"
+
+#Brushing teeth
+puts "clean and white and very nice"
+
+#Take a Shower
+puts "Your squeaky clean now"
+
+#Go to the toilet
+puts "Kek"
+
+#Go to the Dining room
+puts "What would you like to eat"
+puts "Milk and cereal"
+puts "Cereal and milk"
+puts "Waffles"
+puts "a singular whole banana"
+puts "Omlette"
+
+#Every food except banana and waffles
+puts "OMNOMNOM"
+
+#Waffles
+puts "DO YOU LIKE WAFFLES?"
+puts "YEAH WE LIKE WAFFLES!"
+
+#Banana
+puts "You 'accidentally' put the whole banana in your throat and choke and die."
+
+#Leave the Building(without getting dressed)
+puts "You get arrested for public nudity"
+
+#Leave the Building(after getting dressed)
+puts "You leave your house and make your way over to Browning."
+puts "Outside the building you find one of your classmates dawdiling."
+puts "What do you do"
+puts "Go into the building"
+puts "Talk to Ren"
+
+#Talk to Ren
+puts "How do you wanna start"
+puts "Say 'HI'"
+puts "Say 'Wut Up!'"
+puts "Ask him about VGDC"
+puts "Give him a pat on the cheek"
+puts "Ask him for access to his spicy meme chat"
+puts "Go into Browning"
+
+#HI or Wut Up!
+puts "He says 'hey...' back"
+
+#Ask him about VGDC
+puts "He says it was cancelled this week and is occuring next week"
+puts "Response?"
+puts "Inquire that we haven't had a meeting in forever"
+puts "Leave it at that"
+
+#Inquire about VGDC
+puts "He admits thats true... but they had good reason for putting it off for this long."
+puts "Whether it be for holiday or testing"
+puts "Response?"
+puts "Offer to run the club for him"
+puts 'Leave it at that'
+
+#Offer to run the club for him
+puts "He declines since he is the 'one and only leader of the club'"
+puts "However, seeing your optimistic attitude about a club no one takes seriously, he makes you copresident #6"
+
+#Give him a pat on the cheek
+puts "He slaps you so hard back that your head explodes"
+
+#Ask him for spicy meme chat
+puts "He declines"
+
+#Ask him for spicy meme chat (repeated)
+puts "He declines again"
+
+#Spicy meme attempt 3
+puts "He mentions that most the 'memes' are only funny because they're controversial."
+
+#persistence
+puts "He gets tired of you and decides to add you"
+puts "Out of nowhere a bunch of cops come out of some bushes and arrest you both."
+
+#Go into Browning
+puts "You walk through those red doors as Rames, Headmaster of the upper school, grabs your hand into the building"
+
+#SweatShirt gameover
+puts "He notices Your wearing a sweatshirt and instantly gets triggered kicking you out of the building forever."
+
+#Hallway dialogue
+puts "your standing in the hallway, free to explore the building as you please"
+puts "What floor do you go to?"
+puts "Basement"
+puts "Floor 1"
+puts "Floor 2"
+puts "Floor 3"
+puts "Floor 4"
+puts "Floor 5"
+
+#Basement options
+puts "Your in the basement hallway now, do you:"
+puts "Go to West lockeroom"
+puts "Go to East lockeroom"
+puts "Go to gym"
+puts "Go to cafeteria"
+puts "Go to another floor"
+
+#West lockeroom
+puts "You enter the west lockeroom"
+puts "A bulk of your classmates are chillin out"
+puts "Do you:"
+puts "Join the conversation"
+puts "Check your snapchat"
+puts "Play a game on your phone"
+puts "Start a fight"
+
+#East lockeroom
+puts "You Enter the east lockeroom"
+puts "Not much to do here"
+puts "Do you:"
+puts "Check out your locker"
+puts "leave"
+
+#Gym
+puts "You enter the Gym through the two doors leading to it"
+puts "Many of your fellow classmates are dribbling basketballs and shooting hoops"
+puts "Do you:"
+puts "Find a basketball and start making shots with it"
+puts "Challenge the first person you see to a 1-on-1"
+puts "Leave"
+
+#Cafeteria
+puts "You walk down to the cafeteria"
+puts "There are a few seinors having breakfast"
+puts "There is also the usual lunch crew in the kitchen"
+puts "Do you"
+puts "Steal some of the seinor breakfast"
+puts "Small talk the seinors"
+puts "Say hi to Wose, the lunch distributor"
+puts "Get some lunch"
+puts "Leave"
+
+#Floor 1 options
+puts "Your in the 1st floor hallway now, what do you do:"
+puts "Go to the tech lab"
+puts "Go to the library"
+puts "Leave"
+
+#Go to the tech lab
+puts "You arrive at the tech lab"
+puts "Mr. Barbecue is there along with the robotics team working on the robot"
+puts "What do you do:"
+puts "Help them build the robot"
+puts "Help them code the robot"
+puts "Try to build your own machine"
+puts "Try to steal the robot"
+
+#Go to the Library
+puts "You arrive at the library"
+puts "The librarian, Ms. Marshmellow, gives you a cold stare when you enter"
+puts "What do you do:"
+puts "Say Hi to Ms. Marshmellow"
+puts "Go to the quiet section"
+puts "Go to the non-quiet section of the library"
+puts "Go out on the terrace"
+puts "Leave"
+
+#Floor 2 options
+puts "Your in the 2nd floor hallway now, what do you do:"
+puts "Go to the wilson room"
+puts "Go to the Adv. Computer Science room"
+puts "Go to the Latin room"
+puts "Leave"
+
+#Go to the wilson room
+puts "You arrive at the wilson room"
+puts "Mr. Symbols is there playing the piano, what do you do:"
+puts "Start singing to his music"
+puts "Pick up an instrument and start playing it"
+puts "Leave"
+
+#Go to the Adv. Comp Sci room
+puts "You go to the tech lab"
+puts "You find Mr. Foil in an otherwise completely empty room"
+puts "What do you do:"
+puts "Present your text game to him"
+puts "Sit down and do lessons in Ruby the Hard Way"
+puts "Sit down and read WoMD"
+puts "Have a conversation with Mr. Foil"
+puts "Commence a VGDC meeting"
+
+#Go to the Latin room
+puts "You go to the latin room"
+puts "Mr. Old greets you with a salvete as you open the door"
+puts "What do you do:"
+puts "Converse with Mr. Old"
+puts "Leave"
+
+#Floor 3 options
+puts "Your in the 3rd floor hallway now, what do you do:"
+puts "Go to the Chemistry lab"
+puts "Leave"
+
+#Chemisry Lab
+puts "You go t- oh ratz!"
+puts "Freakin Boey is in the front of the door"
+puts "What do you do:"
+puts "Punch him in the face"
+puts "Bribe him"
+puts "Attempt to converse with him"
+puts "Leave"
+
+#Floor 4 options
+puts "Your in the 4th floor hallway now, what do you do:"
+puts "Go to one of the two art rooms"
+puts "Go to the French room"
+puts "Go to the workout room"
+puts "Leave"
+
+#Art room
+puts "You go to an artroom"
+puts "Mr. Pablo and Picasso are painting lavish paintings"
+puts "What do you do:"
+puts "Examine their miraculous painting"
+puts "Make your own drawing"
+
+#French room
+puts "You think about going in there, but nah"
+puts "Theres no reason, like it would probably be just another death sequence"
+puts "What could you possibly achieve by going in there, nothing, thats right"
+
+#Workout room
+puts "You go to the workout room, where a group of coaches are joking around"
+puts "What do you do:"
+puts "Attempt to converse with them"
+puts "Steal their special gear"
+puts "Challenge them to a weight lifting competition"
+
+#Floor 5 options
+puts "Your in the 5th floor hallway now, what do you do:"
+puts "Go to the History room"
+puts "Go to the Math room"
+puts "Go to the English room"
+puts "Go to the Upper gym"
+puts "Leave"
+
+#Go to History Room
+puts "You go to the history room"
+puts "You find a few of your classmates teasing the history teacher, Mr. Quartz"
+puts "What do you do:"
+puts "Fight the teasers"
+puts "Join the teasers"
+puts "Leave"
+
+#Go to Math room
+puts "You go to the math room"
+puts "Mr. Baja Blast fires a math question right at ya:"
+puts "What is log(100)"
+puts "(Put your answer all lowercase rounded to the nearest WHOLE INTEGER no spaces)"
+
+#Go to English room
+puts "You go to the English room"
+puts "Mr. Shetdedoh, the English teacher along with Dr. Rivergate, the other Latin teacher are sitting in the room"
+puts "What do you:"
+puts "Joke with Mr. Shetdedoh"
+puts "Joke with Dr. Rivergate"
+puts "Leave"
+
+puts "You have entered the upper gym"
+puts "Oddly, no ones here..."
+puts "The door to the outside is slightly ajar however"
+puts "Do you:"
+puts "Play Basketball"
+puts "Go through the ajar door"
